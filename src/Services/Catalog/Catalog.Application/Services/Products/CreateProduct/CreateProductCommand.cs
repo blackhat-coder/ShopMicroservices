ee4d01d5afc6f5f3ef918f66350825c6dd08a2b8
Aboh Israel <israel.aboh@flutterwavego.com>
2024-01-24 01:53:31 +0100

    Update default create product

--- src/Services/Catalog/Catalog.Application/Services/Products/CreateProduct/CreateProductCommand.cs
+++ src/Services/Catalog/Catalog.Application/Services/Products/CreateProduct/CreateProductCommand.cs
@@ -1,4 +1,5 @@
-﻿using Catalog.Domain.Common;
+﻿using Catalog.Application.Response;
+using Catalog.Domain.Common;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -8,15 +9,15 @@ using System.Threading.Tasks;
 
 namespace Catalog.Application.Services.Products.CreateProduct
 {
-    public class CreateProductCommand : IRequest<CreateProductResponse>
+    public class CreateProductCommand : IRequest<Result<CreateProductResponse>>
     {
         public string Name { get; set; }
         public string Category { get; set; }
         public string Summary { get; set; }
         public string Description { get; set; }
         public string ImageFile { get; set; }
-        public int stock { get; set; }
-        public decimal Price { get; set; }
-        public string Currency { get; set; }
+        public int stock { get; set; } = 1;
+        public decimal Price { get; set; } = 1;
+        public string Currency { get; set; } = "USD";
     }
 }
